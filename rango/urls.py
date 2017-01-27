dfd2f161c11461f58924687933dbc87bf2e9c2c3
Dominic Small <2195530s@student.gla.ac.uk>
2017-01-27 17:25:43 +0000

    Added templates and static dir. Started serving media

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
-    url(r'^index', views.index, name = 'index'),
+    url(r'^$', views.index, name = 'index'),
     url(r'^about', views.about, name = 'about'),
 ]
--- wad_django_project/urls.py
+++ wad_django_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
@@ -26,4 +28,4 @@ urlpatterns = [
     url(r'^rango/', include('rango.urls')),
 	
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
