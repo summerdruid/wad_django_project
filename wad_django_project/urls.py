3fb1ce10453fa16b8dedeb17e5cae3ba6198d510
Dominic Small <Dominic Small>
2017-01-31 16:16:00 +0000

    Chapter 5 done - models and databases

--- wad_django_project/urls.py
+++ wad_django_project/urls.py
@@ -26,6 +26,5 @@ urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^$', views.about, name = 'about'),
     url(r'^rango/', include('rango.urls')),
-	
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
